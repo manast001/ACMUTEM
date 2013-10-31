f883cb4f60b957e26810f2bea6ad3b1bbaf5390d
Manuel <manast001@gmail.com>
2013-10-31 19:08:52 +0100

    Update weas.cpp

--- weas.cpp
+++ weas.cpp
@@ -1,19 +1,19 @@
 double determinante(double **a,int n)
 {
-  
+  int det,l;
   det=a[1][1];
-  for(k=1;k<=n-2;k++)
+  for(int k=1;k<=n-2;k++)
   {
     l=k+1;
-    for(i=l;i<=n-1;i++)
-    { 
-      for(j=l;j<=n-1;j++)
-      a[i][j] = ( a[k][k]*a[i][j]-a[k][j]*a[i][k] )/a[k][k]; }
+    for(int i=l;i<=n-1;i++)
+    {
+      for(int j=l;j<=n-1;j++)
+      a[i][j] = ( a[k][k]*a[i][j]-a[k][j]*a[i][k] )/a[k][k]; 
+    }
       det=det*a[k+1][k+1];
-    } 
-  }
+   }
+  return det;
 }
-
 long fibonachi(long n)
 {
   if(n<=1)
